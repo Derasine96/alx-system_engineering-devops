211c9e9eedac628389adb1663bfda583b2a76db2
Obikwelu Chidera <chidexobikwelu@gmail.com>
2023-11-28 16:56:51 -0800

    Added join(,)

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-match = ARGV[0].match(/\[from:([^\]]+)\] \[to:(\+\d+)\] \[flags:(.*?)\]/)
-puts "#{match[1]},#{match[2]},#{match[3]}"
+puts ARGV[0].scan(/\[from:([^\]]+)\] \[to:(\+\d+)\] \[flags:(.*?)\]/).join(",")
