7833b65f0db38a1b8edbf2a4f1c9365c083549d5
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-01-19 03:57:40 -0800

    Push all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #Install flask from pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/pip3 show flask | /bin/grep -q "Version: 2.1.0"',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
