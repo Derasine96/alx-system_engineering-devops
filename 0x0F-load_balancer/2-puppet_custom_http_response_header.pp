19d19c6e4684c8f9ede48d167051eda1b50a7d83
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-01-29 15:18:10 -0800

    automate the task of creating a custom HTTP header response, but with Puppet.

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Add a custom HTTP header with Puppet
+
+$nginx_config_content = "server {
+   listen 80 default_server;
+   listen [::]:80 default_server;
+   
+   root /var/www/html;
+   index index.html;
+   location /redirect_me {
+      return 301 https://upschool.com/register;
+   }
+   error_page 404 /404.html;
+   location = /404.html{
+      internal;
+   }
+
+   location / {
+      add_header X-Served-By \$hostname;
+      try_files \$uri \$uri/ =404;
+   }
+}"
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => $nginx_config_content,
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
