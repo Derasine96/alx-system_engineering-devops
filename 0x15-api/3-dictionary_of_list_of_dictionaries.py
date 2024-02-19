f3cbea973f25f711ad091d9bce224dd2f6ad9579
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-02-19 06:44:41 -0800

    REST API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """Exports to-do list information of all employees to JSON format."""
-import json
 import requests
+import json
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    employees = requests.get(url + "users").json()
+    employees_response = requests.get(url + "users")
+    employees = employees_response.json()
+
+    tasks_dict = {}
+    for emp in employees:
+        todos_response = requests.get(url + "todos",
+                                      params={"userId": emp.get("id")})
+        todos = todos_response.json()
+        tasks_list = [
+            {
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "uName": emp.get("username")
+            }
+            for t in todos
+        ]
+        tasks_dict[emp.get("id")] = tasks_list
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        tasks_dict = {}
-        for emp in employees:
-            emp_id = emp.get("id")
-            todos_response = requests.get(url + "todos",
-                                          params={"userId": emp_id})
-            todos = todos_response.json()
-            tasks_list = []
-            for t in todos:
-                todo_dict = {
-                    "task": t.get("title"),
-                    "completed": t.get("completed"),
-                    "uName": emp.get("username")
-                }
-                tasks_list.append(todo_dict)
-            tasks_dict[emp_id] = tasks_list
         json.dump(tasks_dict, jsonfile)
