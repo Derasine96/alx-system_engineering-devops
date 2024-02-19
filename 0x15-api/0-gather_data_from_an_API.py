3274df121880d6e98e79f4a09bccd181af4c7c59
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-02-19 05:52:50 -0800

    REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,26 +4,11 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    emp_id = sys.argv[1]
-    user_url = (
-                f'https://jsonplaceholder.typicode.com/users/{emp_id}'
-                )
-    todo_url = (
-                f'https://jsonplaceholder.typicode.com/users/{emp_id}/todos'
-                )
-    user_response = requests.get(user_url)
-    user_response.raise_for_status()
-    user_dat = user_response.json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user_data = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    t_response = requests.get(todo_url)
-    t_response.raise_for_status()
-    t_data = t_response.json()
-
-    total = len(t_data)
-    done = len([todo for todo in t_data if todo['completed']])
-
-    print("Employee {} is done with tasks({}/{}):".format(user_dat.get('name'),
-                                                          done, total))
-    for todo in t_data:
-        if todo['completed']:
-            print('\t ' + todo['title'])
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_data.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
