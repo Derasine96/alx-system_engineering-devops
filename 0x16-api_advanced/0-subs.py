41e81ac06a0f58f821aa5b19b91108972d112ac9
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-03-05 05:47:38 -0800

    number of subscribers of a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": '0x16-api_advanced:project:v1.0.0(by /u/Derasine96)'
