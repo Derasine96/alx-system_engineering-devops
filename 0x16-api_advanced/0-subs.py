70077c9fcf65a295ef4d861a0c601e6c198cadda
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-03-05 05:55:45 -0800

    number of subscribers of a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": '0x16-api_advanced:project:v1.0.0(by /u/Derasine96)'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 200 and not response.is_redirect:
         results = response.json().get("data")
         return results.get("subscribers")
     return 0
